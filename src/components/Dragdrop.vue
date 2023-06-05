d8ccc2ffd2bca6cfde83ac12573636d6b32d3832
Plamenpowa <pkostadinov72@gmail.com>
2023-06-05 15:18:20 +0300

    removing useless code

--- src/components/Dragdrop.vue
+++ src/components/Dragdrop.vue
@@ -1,96 +1,33 @@
 <template>
-  <div>
-    <div
-      class="drop-zone"
-      @drop="onDrop($event, 1)"
-      @dragover.prevent
-      @dragenter.prevent
-    >
-      <div
-        v-for="item in listOne"
-        :key="item.title"
-        class="drag-el"
-        :draggable="true"
-        @dragstart="startDrag($event, item)"
-      >
-        {{ item.title }}
-      </div>
-    </div>
-    <div
-      class="drop-zone"
-      @drop="onDrop($event, 2)"
-      @dragover.prevent
-      @dragenter.prevent
-    >
-      <div
-        v-for="item in listTwo"
-        :key="item.title"
-        class="drag-el"
-        :draggable="true"
-        @dragstart="startDrag($event, item)"
-      >
-        {{ item.title }}
-      </div>
+  <div
+    class="dropzone"
+    @dragover.prevent
+    @dragenter.prevent
+    @dragleave.prevent
+    @drop.prevent="handleDrop"
+  >
+    <div>
+      <p>Drag and drop files here</p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from "vue";
-
-const items = ref<any[]>([
-  {
-    id: 0,
-    title: "Item A",
-    list: 1,
-  },
-  {
-    id: 1,
-    title: "Item B",
-    list: 1,
-  },
-  {
-    id: 2,
-    title: "Item C",
-    list: 2,
-  },
-  {
-    id: 3,
-    title: "Item D",
-    list: 2,
-  },
-]);
-
-const listOne = computed(() => {
-  return items.value.filter((item) => item.list === 1);
-});
-
-const listTwo = computed(() => {
-  return items.value.filter((item) => item.list === 2);
-});
-
-function startDrag(evt: any, item: any) {
-  evt.dataTransfer.dropEffect = "move";
-  evt.dataTransfer.effectAllowed = "move";
-  evt.dataTransfer.setData("itemID", item.id);
-}
-
-function onDrop(evt: any, list: any) {
-  const itemID = evt.dataTransfer.getData("itemID");
-  const item = items.value.find((item: any) => item.id == itemID);
-  item.list = list;
+function handleDrop(event: any) {
+  event.preventDefault();
+  const files = Array.from(event.dataTransfer.files);
+  // do something with the dropped files
+  console.log(files);
 }
 </script>
 
 <style scoped>
-.drop-zone {
-  background-color: #eee;
-  margin-bottom: 10px;
-  padding: 10px;
-}
-.drag-el {
-  background-color: #fff;
-  margin-bottom: 10px;
-  padding: 5px;
+.dropzone {
+  width: 300px;
+  height: 200px;
+  border: 2px dashed #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>
